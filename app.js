0144673522d37150f38190c700e9fca55c856852
junkai <jacob19971105@gmail.com>
2019-11-11 19:38:59 +0800

    add app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+var http = require("http");
+//  node.js 載入模組
+http.createServer(function(request, response) {
+  console.log(request.url);
+  if(request.url == '/'){
+// 路由
+    // 資料庫
+    console.log("接收到網頁請求！");
+    response.writeHead(200, {"Content-Type": "text/HTML"});
+    response.write("<h1>index</h1>");
+    response.end();
+  }else if(request.url == '/search'){
+    console.log("接收到網頁請求！");
+    response.writeHead(200, {"Content-Type": "text/HTML"});
+    response.write("<h1>search</h1>");
+    response.end();
+  }else if(request.url == '/page1'){
+    console.log("接收到網頁請求！");
+    response.writeHead(200, {"Content-Type": "text/HTML"});
+    response.write("<h1>page1</h1>");
+    response.end();
+  }else if(request.url == '/page2'){
+    console.log("接收到網頁請求！");
+    response.writeHead(200, {"Content-Type": "text/HTML"});
+    response.write("<h1>page2</h1>");
+    response.end();
+  }else if(request.url == '/page3'){
+    console.log("接收到網頁請求！");
+    response.writeHead(200, {"Content-Type": "text/HTML"});
+    response.write("<h1>page3</h1>");
+    response.end();
+  }
+
+  
+}).listen(process.env.PORT ||3000);
+console.log("Server已開啟port: 3000.");
+
